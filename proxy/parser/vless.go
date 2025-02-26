1a4c44946327a84e59b7624c6694d51db2c7c54c
beck-8 <1504068285@qq.com>
2025-02-26 23:44:53 +0800

    fix: 优化vless解析

--- proxy/parser/vless.go
+++ proxy/parser/vless.go
@@ -23,45 +23,42 @@ func ParseVless(data string) (map[string]any, error) {
 		return nil, nil
 	}
 
-	host := hostPort[0]
-	port, err := strconv.Atoi(hostPort[1])
+	port, err := strconv.Atoi(parsedURL.Port())
 	if err != nil {
 		return nil, fmt.Errorf("格式错误: 端口格式不正确")
 	}
 
 	// 解析参数
-	params, err := url.ParseQuery(parsedURL.RawQuery)
-	if err != nil {
-		return nil, nil
-	}
+	query := parsedURL.Query()
 
 	// 构建 clash 格式的代理配置
 	proxy := map[string]any{
-		"name":        parsedURL.Fragment,
-		"type":        "vless",
-		"server":      host,
-		"port":        port,
-		"uuid":        parsedURL.User.String(),
-		"network":     params.Get("type"),
-		"tls":         params.Get("security") == "tls",
-		"servername":  params.Get("sni"),
-		"flow":        params.Get("flow"),
-		"fingerprint": params.Get("fp"),
+		"name":               parsedURL.Fragment,
+		"type":               "vless",
+		"server":             parsedURL.Hostname(),
+		"port":               port,
+		"uuid":               parsedURL.User.String(),
+		"network":            query.Get("type"),
+		"tls":                query.Get("security") != "none",
+		"udp":                query.Get("udp") == "true",
+		"servername":         query.Get("sni"),
+		"flow":               query.Get("flow"),
+		"client-fingerprint": query.Get("fp"),
 	}
 
 	// 添加 ws 特定配置
-	if params.Get("type") == "ws" {
+	if query.Get("type") == "ws" {
 		wsOpts := map[string]any{
-			"path": params.Get("path"),
+			"path": query.Get("path"),
 			"headers": map[string]any{
-				"Host": params.Get("host"),
+				"Host": query.Get("host"),
 			},
 		}
 		proxy["ws-opts"] = wsOpts
 	}
 	realityOpts := map[string]any{
-		"public-key": params.Get("pbk"),
-		"short-id":   params.Get("sid"),
+		"public-key": query.Get("pbk"),
+		"short-id":   query.Get("sid"),
 	}
 	proxy["reality-opts"] = realityOpts
 
