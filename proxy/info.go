ba8963c58380b09e9e61ffb200b414c71e3b8240
beck-8 <1504068285@qq.com>
2025-06-22 19:34:08 +0800

    fix: fix GetProxyCountry

--- proxy/info.go
+++ proxy/info.go
@@ -15,11 +15,16 @@ import (
 
 func GetProxyCountry(httpClient *http.Client) (loc string, ip string) {
 	for i := 0; i < config.GlobalConfig.SubUrlsReTry; i++ {
-		loc, ip = GetEdgeOneProxy(httpClient)
+		loc, ip = GetIPCheckProxy(httpClient)
 		if loc != "" && ip != "" {
 			return
 		}
 		loc, ip = GetCFProxy(httpClient)
+		if loc != "" && ip != "" {
+			return
+		}
+		// 不准
+		loc, ip = GetEdgeOneProxy(httpClient)
 		if loc != "" && ip != "" {
 			return
 		}
@@ -28,13 +33,13 @@ func GetProxyCountry(httpClient *http.Client) (loc string, ip string) {
 }
 
 func GetEdgeOneProxy(httpClient *http.Client) (loc string, ip string) {
-	type Geo struct {
-		CountryCodeAlpha2 string `json:"countryCodeAlpha2"`
-	}
-
-	type EO struct {
-		Geo      Geo    `json:"geo"`
-		ClientIp string `json:"clientIp"`
+	type GeoResponse struct {
+		Eo struct {
+			Geo struct {
+				CountryCodeAlpha2 string `json:"countryCodeAlpha2"`
+			} `json:"geo"`
+			ClientIp string `json:"clientIp"`
+		} `json:"eo"`
 	}
 
 	url := "https://functions-geolocation.edgeone.app/geo"
@@ -57,14 +62,14 @@ func GetEdgeOneProxy(httpClient *http.Client) (loc string, ip string) {
 		return
 	}
 
-	var eo EO
+	var eo GeoResponse
 	err = json.Unmarshal(body, &eo)
 	if err != nil {
 		slog.Debug(fmt.Sprintf("解析edgeone JSON 失败: %v", err))
 		return
 	}
 
-	return eo.Geo.CountryCodeAlpha2, eo.ClientIp
+	return eo.Eo.Geo.CountryCodeAlpha2, eo.Eo.ClientIp
 }
 
 func GetCFProxy(httpClient *http.Client) (loc string, ip string) {
@@ -98,4 +103,41 @@ func GetCFProxy(httpClient *http.Client) (loc string, ip string) {
 		}
 	}
 	return
+}
+
+func GetIPCheckProxy(httpClient *http.Client) (loc string, ip string) {
+	type GeoIPData struct {
+		IP      string `json:"ip"`
+		Country string `json:"country"`
+	}
+
+	url := "https://64.ipcheck.ing/geo"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		slog.Debug(fmt.Sprintf("创建请求失败: %s", err))
+		return
+	}
+	req.Header.Set("User-Agent", convert.RandUserAgent())
+	resp, err := httpClient.Get(url)
+	if err != nil {
+		slog.Debug(fmt.Sprintf("ipcheck.ing获取节点位置失败: %s", err))
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+
+		slog.Debug(fmt.Sprintf("ipcheck.ing读取节点位置失败: %s", err))
+		return
+	}
+
+	var geo GeoIPData
+	err = json.Unmarshal(body, &geo)
+	if err != nil {
+		slog.Debug(fmt.Sprintf("解析ipcheck.ing JSON 失败: %v", err))
+		return
+	}
+
+	return geo.Country, geo.IP
 }
